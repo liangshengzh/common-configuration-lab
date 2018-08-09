7dbd4f78f88b7c02ff1c0dc2685c1f5fcbc84df0
jimmy zhong <jimmy.zhong@ximalaya.com>
2018-08-09 10:02:59 +0800

    add xml example

--- src/main/java/cn/devlab/Database.java
+++ src/main/java/cn/devlab/Database.java
@@ -1,19 +1,45 @@
 package cn.devlab;
 
-import java.io.File;
+import java.util.List;
 import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 
 /**
- * Hello world!
+ *
  */
 public class Database {
 
   public static void main(String[] args) throws Exception {
     Configurations configurations = new Configurations();
 
-    Configuration configuration = configurations.properties(new File("db.properties"));
+    FileBasedConfigurationBuilder<PropertiesConfiguration> builder = configurations.propertiesBuilder("db.properties");
+    //Configuration configuration = configurations.properties(new File("db.properties"));
+    Configuration configuration = builder.getConfiguration();
     String host = configuration.getString("db.host");
-    System.out.println(host);
+    int port = configuration.getInt("db.port");
+    System.out.println(host + ":" + port);
+
+
+    //xml
+
+    XMLConfiguration xmlConfiguration = configurations.xml("paths.xml");
+
+    String stage = xmlConfiguration.getString("processing[@stage]");
+    System.out.println(stage);
+
+    List<String> pathList = xmlConfiguration.getList(String.class, "processing.paths.path");
+
+    System.out.println(pathList);
+
+
+    //update
+
+    configuration.addProperty("db.type","production");
+
+
+    builder.save();
   }
 }
